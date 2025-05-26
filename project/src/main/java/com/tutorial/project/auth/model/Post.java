82bc831e576d7c85cf2d9f9ac009b9c69fac9023
Izzatbek <abdusharipovizzat03@gmail.com>
2025-05-26 13:58:35 +0500

    blog platform

--- project/src/main/java/com/tutorial/project/auth/model/Post.java
+++ project/src/main/java/com/tutorial/project/auth/model/Post.java
@@ -7,6 +7,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(name = "posts")
@@ -19,4 +21,30 @@ public class Post implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  Long id;
 
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private String author;
+
+    private String image;
+
+    private String videoUrl;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "post",cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<Comment> comments;
+
+    @PrePersist
+    protected void oneCreate(){
+        this.createdAt=this.updatedAt=LocalDateTime.now();
+    }
+    @PreUpdate
+    protected void oneUpdate(){
+        this.updatedAt=LocalDateTime.now();
+    }
 }
